5569757fa68afb47ee1545d453c90b52e14d8aac
aykhan.dev <aykhan.abdullazade@gmail.com>
2022-12-27 09:42:07 +0300

    fix store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,13 +6,12 @@ import {
 import registrationPageReducer from "../containers/RegistrationPage/store/slice";
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
-import saga from "./saga";
 
 const rootReducer = combineReducers({
   registrationPageReducer,
 });
 
-const sagaMiddleware = createSagaMiddleware();
+export const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [
   ...getDefaultMiddleware({ thunk: false }),
@@ -26,10 +25,6 @@ export const setupStore = () => {
     middleware,
   });
 };
-
-export default setupStore();
-
-sagaMiddleware.run(saga);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
